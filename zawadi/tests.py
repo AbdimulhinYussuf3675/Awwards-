e382056fc6606d56a103eb7371b2c49f797a762e
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-18 20:12:00 +0300

    tested some behaviours

--- zawadi/tests.py
+++ zawadi/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import *
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+
+
+class TestProfile(TestCase):
+  '''
+  class that will test the profile model
+  '''
+  def setUp(self):
+    pass
+  def test_save_profile(self):
+    pass
+  def test_update_bio(self):
+    pass
+
