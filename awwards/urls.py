9721e7ba135600f2bff45f6480863ee8df9a80ae
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-18 20:07:10 +0300

    configured urls

--- awwards/urls.py
+++ awwards/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views 
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'',include('zawadi.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
