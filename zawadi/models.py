4adc406e45e8ba5568e7e6980173c861566e0a8a
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-18 20:09:47 +0300

    added a profile model

--- zawadi/models.py
+++ zawadi/models.py
@@ -1,75 +1,56 @@
 from django.db import models
-from  cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
-from django.urls import reverse
-from django.core.validators import MaxValueValidator, MinValueValidator
 import datetime as dt
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django_countries.fields import CountryField
 
 
 # Create your models here.
 
 class Profile(models.Model):
-    user=models.OneToOneField(User,null=True, on_delete=models.CASCADE)
-    bio= models.CharField(max_length=100)
-    prof_pic= CloudinaryField('image')
+  profile_pic = models.ImageField(default='../static/images/default.jpg',upload_to='media/')
+  bio = models.TextField()
+  user = models.OneToOneField(User,on_delete = models.CASCADE)
+  
+  @receiver(post_save , sender = User)
+  def create_profile(instance,sender,created,**kwargs):
+    if created:
+      Profile.objects.create(user = instance)
 
-    def __str__(self):
-        return f'{self.user.username} Profile'
+  @receiver(post_save,sender = User)
+  def save_profile(sender,instance,**kwargs):
+    instance.profile.save()
+    
+class Projects(models.Model):
+    title = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='media/')
+    project_description = models.TextField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE, blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    site = models.URLField()
+    country = CountryField(default='NBO')
 
-    def save(self):
-        super().save()
-
-    def save_profile(self):
+    def save_project(self):
         self.save()
     
-    def delete_profile(self):
-        self.delete()
-
-    def profiles_posts(self):
-        return self.image_set.all()
+    @classmethod
+    def get_all_projects(cls):
+        projects = Projects.objects.all()
+        return projects
+        
+    @classmethod
+    def get_post(cls, id):
+        projects = Projects.objects.filter(user=id)
+        return projects
 
     @classmethod
-    def search_profile(cls, name):
-        return cls.objects.filter(user__username__icontains=name).all()
+    def search_by_title(cls,search_term):
+        projects = cls.objects.filter(title__icontains=search_term)
+        return projects   
 
-class Project(models.Model):
-    image= CloudinaryField('image')
-    author= models.ForeignKey(Profile, on_delete=models.CASCADE,default=None, null=True, blank=True)
-    title= models.CharField(max_length=30)
-    description= models.CharField(max_length=50)
-    url=models.URLField()
-    likes=models.ManyToManyField(User, related_name='blog_posts')
-    pub_date=models.DateTimeField(auto_now_add=True)
-    user=models.OneToOneField(User,null=True, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-    def total_likes(self):
-        return self.likes.count()
-    
-    def save_image(self):
-        self.save()
-    
-    def delete_image(self):
-        self.delete()
-    
-    def update_image(self):
-        self._do_update()
-
-    @classmethod
-    def user_pics(cls,user):
-        user_pic = cls.objects.filter(user = user)
-        return user_pic
-
-    
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk':self.pk})
-
-class Rating(models.Model):
-    project=models.ForeignKey(Project, on_delete=models.CASCADE)
-    author= models.ForeignKey(Profile, on_delete=models.CASCADE)
-    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     class Meta:
-        unique_together=(('author', 'project'))
-        index_together=(('author', 'project'))
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.title 
