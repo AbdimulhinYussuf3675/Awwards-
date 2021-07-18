6a6af7a6dd2206218d577eda55a779db59e9b89c
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-18 09:12:35 +0300

    added profile model

--- zawadi/models.py
+++ zawadi/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from  cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.core.validators import MaxValueValidator, MinValueValidator
+import datetime as dt
+
 
 # Create your models here.
+
+class Profile(models.Model):
+    user=models.OneToOneField(User,null=True, on_delete=models.CASCADE)
+    bio= models.CharField(max_length=100)
+    prof_pic= CloudinaryField('image')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+    def save_profile(self):
+        self.save()
+    
+    def delete_profile(self):
+        self.delete()
+
+    def profiles_posts(self):
+        return self.image_set.all()
+
+    @classmethod
+    def search_profile(cls, name):
+        return cls.objects.filter(user__username__icontains=name).all()
+
+class Project(models.Model):
+    image= CloudinaryField('image')
+    author= models.ForeignKey(Profile, on_delete=models.CASCADE,default=None, null=True, blank=True)
+    title= models.CharField(max_length=30)
+    description= models.CharField(max_length=50)
+    url=models.URLField()
+    likes=models.ManyToManyField(User, related_name='blog_posts')
+    pub_date=models.DateTimeField(auto_now_add=True)
+    user=models.OneToOneField(User,null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def total_likes(self):
+        return self.likes.count()
+    
+    def save_image(self):
+        self.save()
+    
+    def delete_image(self):
+        self.delete()
+    
+    def update_image(self):
+        self._do_update()
+
+    @classmethod
+    def user_pics(cls,user):
+        user_pic = cls.objects.filter(user = user)
+        return user_pic
+
+    
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk':self.pk})
+
+class Rating(models.Model):
+    project=models.ForeignKey(Project, on_delete=models.CASCADE)
+    author= models.ForeignKey(Profile, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    class Meta:
+        unique_together=(('author', 'project'))
+        index_together=(('author', 'project'))
