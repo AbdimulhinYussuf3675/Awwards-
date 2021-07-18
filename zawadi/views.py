a1953e49a024bf57ff96c9f1572834bc0a30a1f5
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-18 09:13:13 +0300

    added a login view

--- zawadi/views.py
+++ zawadi/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@loging_required(login_url='/accounts/login/')
+def welcome(request):
+    current_user = request.user
+    project = Projects.get_all_projects()
+    return render (request, 'home.html', {"projects":projects})
+    
