6ac7fe05428036425166afb417f9fb257a6233be
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-18 20:19:46 +0300

    set timezone

--- awwards/settings.py
+++ awwards/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'awwards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awards',
+        'USER': 'moringa',
+    'PASSWORD':'access',
     }
 }
 
@@ -109,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
